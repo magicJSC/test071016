6f664ecd25a57979c7577e2f818180edc382cec5
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-22 15:33:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.text("hello world")
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# 데이터 불러오기
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
